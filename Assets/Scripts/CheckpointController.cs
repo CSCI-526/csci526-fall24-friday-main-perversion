ba448e2712bcee95ebc908b5b03dc863067564cd
Vibhav2001 <schirrav@usc.edu>
2024-10-24 04:14:19 -0700

    Update CheckpointController.cs

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckpointController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Material color1;
+    public Image targetImage;
     //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
     //Material sphereMaterial = sphere.GetComponent<Renderer>().material;
     public bool checkpointReached;
@@ -25,6 +27,7 @@ public class CheckpointController : MonoBehaviour
         if (other.tag == "Player")
         {
             GetComponent<Renderer>().material.color = Color.blue;
+            targetImage.color = Color.blue;
             checkpointReached = true;
         }
     }
